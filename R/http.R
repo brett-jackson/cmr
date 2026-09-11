b58f73218612410edf4a0af0d0e8072b32579c1c
brett <b.jackson.eng@gmail.com>
2026-09-11 15:12:03 -0600

    query structure

--- R/http.R
+++ R/http.R
@@ -1,8 +1,40 @@
-cmr_query <- function()
+# Form base API request, or return piped request back
+request_base <- function( req = NULL )
 {
-    req <- httr2::request(CMR_ENDPOINT)
-    req |> 
-        httr2::req_url_path_append("search","collections") |>
-        httr2::req_headers(Authorization = Sys.getenv("EDL_TOKEN"))
+    # Create request if nothing was piped
+    if ( is.null(req) )
+    {
+        # Create default request with CMR endpoint and EDL token
+        req <- httr2::request(CMR_ENDPOINT)|> 
+            httr2::req_url_path_append("search") |>
+            httr2::req_headers(Authorization = Sys.getenv("EDL_TOKEN"))
+    }
+    return(req)
+}
+
+
+# Form request for concept id
+request_concept <- function( req = NULL, concept_id )
+{
+    # Form base request if NULL
+    req <- req |> request_base()
+}
+
+# Create request with temporal range or add temporal parameter to existing request
+request_temporal <- function( req = NULL, time_range )
+{
+    # Check validity of temporal range parameter
+    stopifnot(
+        "time_range must be a string" = is.charcter(time_range)
+    )
+    
+    # Form request
+    req <- req |> 
+        request_base() |> # Form base request if not provided by pipe
+        req_url_query('temporal',time_range) # Add temporal parameter to url
+}
+# Send a CMR request
+send_request <- function( req )
+{
     httr2::req_perform(req)
 }
